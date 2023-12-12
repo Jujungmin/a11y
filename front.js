0f9bd454ef933ea93beb14501256b0f134f4d281
jungmin <43zuhzuh@gmail.com>
2023-12-12 17:52:13 +0900

    JS script 수정완료

--- front.js
+++ front.js
@@ -29,13 +29,36 @@
 });
 */
 
+// 23.12.11 : JS 진행 후 vue 로 다시 진행해보기
 document.addEventListener('DOMContentLoaded', function() {
     const $areaTab = document.querySelectorAll('.area-tab');
     if($areaTab.length > 0) {
-        const $tabMenu = document.querySelectorAll('.tab-menu > li');
+        const $tabBtn = document.querySelectorAll('.tab-btn');
 
-        $tabMenu.forEach((el,i) => {
-            console.log(el,i)
+        $tabBtn.forEach((_this,i,arr) => {
+            _this.addEventListener('click', function() {
+                const tabBtnId = _this.getAttribute('href').slice(1);
+                const $tabPanel = document.getElementById(tabBtnId);
+                const tabPanelAll = _this.closest('.area-tab').nextElementSibling.children;
+                
+                // _this.parentElement.classList.add('is-active');
+                // _this.closest('.tab-menu').querySelector('li').classList.remove('is-active');
+                let tabLi = _this.closest('.tab-menu').querySelectorAll('li');
+                // console.log(tabLi.length)
+
+                for(let i = 0; i < tabLi.length; i++) {
+                    tabLi[i].classList.remove('is-active');
+                }
+                _this.parentElement.classList.add('is-active');
+                
+                for(let i = 0; i < tabPanelAll.length; i++) {
+                    // console.log(i)
+                    const $tabPanel = tabPanelAll[i];
+                    $tabPanel.classList.remove('is-block');
+                }
+                $tabPanel.classList.add('is-block');
+                return false;
+            })
         });
     }
 })
